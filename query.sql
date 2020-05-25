b74f10ad20312b9a379c58903467ff3613c82c4c
mengyan0803 <57573785+mengyan0803@users.noreply.github.com>
2020-05-24 22:35:31 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,34 +1,4 @@
-﻿Select max(high) as Hourly_high,name,DATE_TRUNC('hour',
-   Coalesce(
-     try(date_parse(myh_yf_streamdata.ts, '%Y-%m-%d %H:%i:%s')),
-     try(date_parse(myh_yf_streamdata.ts, '%Y/%m/%d %H:%i:%s')),
-     try(date_parse(myh_yf_streamdata.ts, '%d %M %Y %H:%i:%s')),
-     try(date_parse(myh_yf_streamdata.ts, '%d/%m/%Y %H:%i:%s')),
-     try(date_parse(myh_yf_streamdata.ts, '%d-%m-%Y %H:%i:%s')),
-     try(date_parse(myh_yf_streamdata.ts, '%Y-%m-%d')),
-     try(date_parse(myh_yf_streamdata.ts, '%Y/%m/%d')),
-     try(date_parse(myh_yf_streamdata.ts, '%d %M %Y'))
-    ))as hour
+Select max(high) as Hourly_high,name,DATE_TRUNC('hour',date_parse(myh_yf_streamdata.ts, '%Y-%m-%d %H:%i:%s'))as hour
 From myh_yf_streamdata
-group by name,DATE_TRUNC('hour',
-   Coalesce(
-     try(date_parse(myh_yf_streamdata.ts, '%Y-%m-%d %H:%i:%s')),
-     try(date_parse(myh_yf_streamdata.ts, '%Y/%m/%d %H:%i:%s')),
-     try(date_parse(myh_yf_streamdata.ts, '%d %M %Y %H:%i:%s')),
-     try(date_parse(myh_yf_streamdata.ts, '%d/%m/%Y %H:%i:%s')),
-     try(date_parse(myh_yf_streamdata.ts, '%d-%m-%Y %H:%i:%s')),
-     try(date_parse(myh_yf_streamdata.ts, '%Y-%m-%d')),
-     try(date_parse(myh_yf_streamdata.ts, '%Y/%m/%d')),
-     try(date_parse(myh_yf_streamdata.ts, '%d %M %Y'))
-    ))
-order by name,DATE_TRUNC('hour',
-   Coalesce(
-     try(date_parse(myh_yf_streamdata.ts, '%Y-%m-%d %H:%i:%s')),
-     try(date_parse(myh_yf_streamdata.ts, '%Y/%m/%d %H:%i:%s')),
-     try(date_parse(myh_yf_streamdata.ts, '%d %M %Y %H:%i:%s')),
-     try(date_parse(myh_yf_streamdata.ts, '%d/%m/%Y %H:%i:%s')),
-     try(date_parse(myh_yf_streamdata.ts, '%d-%m-%Y %H:%i:%s')),
-     try(date_parse(myh_yf_streamdata.ts, '%Y-%m-%d')),
-     try(date_parse(myh_yf_streamdata.ts, '%Y/%m/%d')),
-     try(date_parse(myh_yf_streamdata.ts, '%d %M %Y'))
-    )) asc
+group by name,DATE_TRUNC('hour',date_parse(myh_yf_streamdata.ts, '%Y-%m-%d %H:%i:%s'))
+order by name,DATE_TRUNC('hour',date_parse(myh_yf_streamdata.ts, '%Y-%m-%d %H:%i:%s')) asc
